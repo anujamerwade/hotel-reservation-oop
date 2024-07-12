3fdef0e1ec8c7617be94a025bf3d38299a590c4e
anujamerwade <anujamerwade97@gmail.com>
2024-07-12 12:58:52 -0400

    Add credit card validation

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pandas as pd
 hotels = pd.read_csv("hotels.csv", dtype={"id": str})
 print(hotels)
 
+cards = pd.read_csv("cards.csv", dtype=str).to_dict(orient="records")
+
 # class User:
 #     # no methods associated with this class, so you can remove it
 
@@ -31,14 +33,28 @@ class Reservation:
         content = f"Reservation done for {self.customer_name} at {self.hotel.hotel_name}"
         return content
 
+class CreditCard:
+    def __init__(self, number):
+        self.number = number
+
+    def validate(self, expiration, holder, cvc):
+        card_data = {"number": self.number, "expiration": expiration, "holder": holder, "cvc": cvc}
+        return card_data in cards
+
+
+
 hotel_ID = input("Enter hotel ID: ")
 hotel = Hotel(hotel_ID)
 
 if hotel.available():
-    hotel.book()
-    name = input("Enter your name: ")
-    reservation = Reservation(hotel_obj=hotel, customer_name=name)
-    print(reservation.generate())
+    credit_card = CreditCard(number="1234")
+    if credit_card.validate(expiration="12/26", holder="JOHN SMITH", cvc="123"):
+        hotel.book()
+        name = input("Enter your name: ")
+        reservation = Reservation(hotel_obj=hotel, customer_name=name)
+        print(reservation.generate())
+    else:
+        print("payment did not go through")
 else:
     print("hotel is not available")
 
